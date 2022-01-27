e998b5c0f148c5bb32ad05730e8e7f88c7462a28
Jasper Oudemans <jasperoudemans@gmail.com>
2022-01-27 16:14:33 -0700

    got database going

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+USE employee_management_db;
+
+INSERT INTO department (name)
+VALUES ('clothing'), ('jewelery'), ('electronics');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('manager', 50000, 1), 
+('clerk', 40000, 1), 
+('assistant', 40000, 2), 
+('cashier', 40000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jeff', 'Hazlett', 1, NULL),
+('Shadale', 'Walker', 2, 1),
+('Frank', 'Sinatra', 3, 1),
+('Sally', 'Seashell', 4, NULL);
+
+
